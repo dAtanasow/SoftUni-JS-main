c56c5121daf6abed237aac4a909970dfc4ef22c9
Denis <denisatanasov76@gmail.com>
2024-05-02 10:47:20 +0300

    add classes

--- Advanced/Classes/tickets.js
+++ Advanced/Classes/tickets.js
@@ -1,26 +1,35 @@
-class Ticket {
-    destination;
-    price;
-    status;
 
-    constructor(destination, price, status) {
-        this.destination = destination;
-        this.price = price;
-        this.status = status;
+
+function getTickets(arr, sortCriteria) {
+
+    class Ticket {
+        destination;
+        price;
+        status;
+    
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
     }
-}
 
-function getTickets(arr) {
     let list = []
     for (let ticket of arr) {
         let [destination, price, status] = ticket.split("|")
-        list.push(new Ticket(destination, price, status))
+        list.push(new Ticket(destination, Number(price), status))
     }
 
-   return list
+    if (sortCriteria === 'price') {
+        list.sort((a, b) => a[sortCriteria] - b[sortCriteria])
+    } else {
+        list.sort((a, b) => a[sortCriteria].localeCompare(b[sortCriteria]))
+    }
+
+    return list
 }
 
 console.log(getTickets(['Philadelphia|94.20|available',
-'New York City|95.99|available',
-'New York City|95.99|sold',
-'Boston|126.20|departed'],))
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'], 'status'))
