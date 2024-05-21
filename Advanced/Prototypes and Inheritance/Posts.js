33df4e652f0e8992a37b7f3b737f1d2fe866ffca
Denis <denisatanasov76@gmail.com>
2024-05-21 18:03:06 +0300

    add description of Posts.js

--- Advanced/Prototypes and Inheritance/Posts.js
+++ Advanced/Prototypes and Inheritance/Posts.js
@@ -1,3 +1,32 @@
+// You need to create several classes for Posts.
+// Implement the following classes:
+// ⦁	Post, which is initialized with the title (String) and content (String)
+// ⦁	The 2 arguments should be public members
+// ⦁	The Post class should also have toString() function which returns the following result:
+// "Post: {postTitle}"
+// "Content: {postContent}"
+// ⦁	SocialMediaPost, which inherits the Post class and should be initialized with 2 additional arguments - likes (Number) and dislikes (Number). The class should hold:
+// ⦁	comments(Strings) -  an array of strings
+// ⦁	addComment(comment)- a function, which adds comments to that array
+// ⦁	The class should extend the toString() function of the Post class, and should return the following result:
+// "Post: {postTitle}"
+// "Content: {postContent}"
+// "Rating: {postLikes - postDislikes}"
+// "Comments:"
+// " * {comment1}"
+// " * {comment2}"
+// . . .
+// In case there are no comments, return information only about the title, content, and rating of the post.
+// ⦁	BlogPost, which inherits the Post class:
+// ⦁	The BlogPost class should be initialized with 1 additional argument - views(Number).
+// ⦁	The BlogPost class should hold 
+// - view() - which increments the views of the object with 1, every time it is called. The function should return the object so that chaining is supported.
+// ⦁	The BlogPost class should extend the toString() function of the Post class, and should return the following result:
+// "Post: {postTitle}"
+// "Content: {postContent}"
+// "Views: {postViews}"
+
+
 function solution() {
     class Post {
         constructor(title, content) {
