e76276d321915c6a0ff535f0de6038370f0ee95e
Denis <denisatanasov76@gmail.com>
2024-06-25 19:20:59 +0300

    add

--- Applications/Remote data and Authentication/02.Phonebook/app.js
+++ Applications/Remote data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("btnLoad").addEventListener("click", onLoad);
+    document.getElementById("btnCreate").addEventListener("click", onCreate);
+    const container = document.getElementById("phonebook");
+
+    const url = "http://localhost:3030/jsonstore/phonebook";
+
+    async function onLoad(e) {
+        const response = await fetch(url);
+        const data = await response.json();
+        container.innerHTML = "";
+        const usersInfo = Object.values(data)
+        Object.values(usersInfo).forEach(rec => {
+            createAndAppendLi(rec);
+        })
+    }
+
+    async function onCreate(e) {
+        const personRef = document.getElementById("person");
+        const phoneRef = document.getElementById("phone");
+        const person = personRef.value;
+        const phone = phoneRef.value;
+        const data = {
+            method: "POST",
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({ person: person, phone: phone })
+        }
+        await fetch(url, data)
+        onLoad();
+        personRef.value = ""
+        phoneRef.value = ""
+    }
+    function createAndAppendLi(data) {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.textContent = "Delete";
+        btn.dataset.id = data._id
+        btn.addEventListener("click", onDelete);
+        li.textContent = `${data.person}: ${data.phone}`;
+        li.appendChild(btn)
+        container.appendChild(li)
+    }
+
+    async function onDelete(e) {
+        const id = e.target.dataset.id;
+        await fetch(url + '/' + id, { method: "DELETE" })
+        onLoad();
+    }
 }
 
 attachEvents();
--- Applications/Remote data and Authentication/03.Students/app.js
+++ Applications/Remote data and Authentication/03.Students/app.js
@@ -1,1 +1,55 @@
-console.log('TODO...');
+async function getStudentInfo() {
+    const formRef = document.getElementById("form");
+    const url = "http://localhost:3030/jsonstore/collections/students"
+
+    const container = document.createElement("div");
+    container.classList.add("students");
+
+    const response = await fetch(url);
+    const data = await response.json();
+    Object.values(data).forEach(rec => {
+        let studentInfo = createRecord(rec);
+        container.appendChild(studentInfo);
+    })
+
+    formRef.appendChild(container)
+
+    function createRecord(data) {
+        const div = document.createElement("div");
+        div.innerHTML = `<input type="text" name="firstName" value=${data.firstName}>
+            <input type="text" name="lastName" value=${data.lastName}>
+            <input type="text" name="facultyNumber" value=${data.facultyNumber}>
+            <input type="text" name="grade"  value=${data.grade}>`;
+
+        return div;
+    }
+
+    document.getElementById("submit").addEventListener("click", onSubmit);
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        let firstNameRef = document.querySelector("input[name='firstName']");
+        let lastNameRef = document.querySelector("input[name='lastName']");
+        let facultyNumberRef = document.querySelector("input[name='facultyNumber']");
+        let gradeRef = document.querySelector("input[name='grade']");
+
+        let firstName = firstNameRef.value;
+        let lastName = lastNameRef.value;
+        let facultyNumber = facultyNumberRef.value;
+        let grade = gradeRef.value;
+        let data = {
+            firstName,
+            lastName,
+            facultyNumber,
+            grade
+        }
+        let div = createRecord(data);
+        container.appendChild(div);
+        firstNameRef.value = '';
+        lastNameRef.value = '';
+        facultyNumberRef.value = '';
+        gradeRef.value = '';
+
+    }
+}
+getStudentInfo();
