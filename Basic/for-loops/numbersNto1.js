5f37ced5fdf27003d06ff8c9909ce634f6952f5a
Denis <denisatanasov76@gmail.com>
2024-08-23 16:52:44 +0300

    Delete Basic directory

--- Basic/for-loops/numbersNto1.js
+++
@@ -1,8 +0,0 @@
-function numbersNto1(input) {
-    let n = Number(input[0]);
-
-    for (i = n; i > 0; i--) {
-        console.log(i);
-    }
-}
-numbersNto1('5')
--- Basic/nestedLoops/clock.js
+++
@@ -1,9 +0,0 @@
-function clock(input) {
-    for (h = 0; h < 24; h++) {
-        for (m = 0; m < 60; m++) {
-            console.log(`${h}:${m}`);
-        }
-    }
-}
-
-clock()
